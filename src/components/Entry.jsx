536aea1daf143b098f3746866194e1ca95602c78
Akshat <masterakshata@gmail.com>
2021-04-25 05:28:46 +0530

    change entry layout and styles

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,14 +1,14 @@
 import AddTask from "./addTask";
 import TaskList from "./TaskList"
 
-const Entry = ({tasks,id,date}) =>{
-    return(<>
+const Entry = ({ tasks, id, date }) => {
+    return (<>
         <div className="entry" key={id}>
-        <h1>{date}</h1>
-            <TaskList tasks={tasks} entryId={id}/>
+            <AddTask entryId={id} />
+            <h1>{date}</h1>
+            <TaskList tasks={tasks} entryId={id} />
         </div>
-        <AddTask entryId={id}/>
-        </>
+    </>
     )
 
 
