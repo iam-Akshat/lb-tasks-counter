505e99cb79195cee1c52ea9c163f05b1f32c9932
Akshat <masterakshata@gmail.com>
2021-05-05 00:52:20 +0530

    add copy stats options

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,11 +1,12 @@
 import AddTask from "./addTask";
+import TaskActions from "./TaskActions";
 import TaskList from "./TaskList"
 
 const Entry = ({ tasks, id, date }) => {
     return (<>
         <div className="entry" key={id}>
             <AddTask entryId={id} />
-            <h1>{date}</h1>
+            <h1>{date}</h1> <TaskActions entryId={id} />
             <TaskList tasks={tasks} entryId={id} />
         </div>
     </>
---
+++ src/components/TaskActions.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { useSelector } from "react-redux"
+
+
+const countMinOrTasks = (taskType, numOrMin, tasks) => {
+    const isMin = "min" === numOrMin
+    const expTasks = tasks.filter(task => task.type === taskType);
+    const result = expTasks.reduce((count, curTask) => {
+        if (isMin) {
+            count += curTask.count * +curTask.duration
+            return count;
+        }
+        count += curTask.count;
+        return count;
+    }, 0);
+    return result;
+}
+const TaskActions = ({ entryId }) => {
+    const [showCopied,setShowCopied] = useState(false);
+    const entries = useSelector((state) => state.entries.entries);
+    const currentEntryTasks = entries.filter(entry => entry.id === entryId)[0].tasks;
+
+    const copy = (target) => {
+        setShowCopied(true)
+        setTimeout(()=>{
+            setShowCopied(false)
+        },1000)
+        switch (target.dataset.action) {
+            case 'expTasks': {
+                const expTasks = countMinOrTasks("EXP", "num", currentEntryTasks)
+                return navigator.clipboard.writeText(expTasks);
+            }
+            case 'expMin': {
+                const expTasks = countMinOrTasks("EXP", "min", currentEntryTasks)
+                return navigator.clipboard.writeText(expTasks);
+            }
+            case 'sxsTasks': {
+                const expTasks = countMinOrTasks("SXS", "num", currentEntryTasks)
+                return navigator.clipboard.writeText(expTasks);
+            }
+            case 'sxsMin': {
+                const expTasks = countMinOrTasks("SXS", "min", currentEntryTasks)
+                return navigator.clipboard.writeText(expTasks);
+            }
+            default:
+                break;
+        }
+    }
+
+    return (
+        <>
+        <h4>Actions</h4>
+        {showCopied && <div className="copy_msg">Copied to clipboard</div>}
+            <div className="actions_holder">
+            <div>
+                <button type="submit" onClick={(e) => { copy(e.target) }} data-action="expTasks">
+                    Total EXP tasks
+                </button>
+            </div>
+            <div>
+                <button type="submit" onClick={(e) => { copy(e.target) }} data-action="expMin">
+                    Total EXP minutes
+                </button>
+            </div>
+            <div>
+                <button type="submit" onClick={(e) => { copy(e.target) }} data-action="sxsTasks">
+                    Total SXS tasks
+                </button>
+            </div>
+            <div>
+                <button type="submit" onClick={(e) => { copy(e.target) }} data-action="sxsMin">
+                    Total SXS minutes
+                </button>
+            </div>
+        </div>
+        </>
+        
+    )
+}
+
+export default TaskActions;
